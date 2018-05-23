24a8bb319a8cc89835407e666369a96c475825cb
Johny Thomas <johnythomas007@gmail.com>
2018-05-23 09:46:34 +0530

    Feat: Add Tab navigator in home screen

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { View, StatusBar } from "react-native"
 import { Constants } from "expo"
+import { createMaterialTopTabNavigator } from "react-navigation"
+import { Ionicons } from "@expo/vector-icons"
 import Decks from "./components/Decks"
-import { primary } from "./utils/colors"
+import { primary, black, white, lightText } from "./utils/colors"
 import Deck from "./components/Deck"
 import Quiz from "./components/Quiz"
 import NewDeck from "./components/NewDeck"
@@ -19,10 +21,45 @@ AppStatusBar.propTypes = {
   backgroundColor: PropTypes.string.isRequired
 }
 
+const Navigation = createMaterialTopTabNavigator(
+  {
+    Decks: {
+      screen: Decks
+    },
+    AddDeck: {
+      screen: NewDeck,
+      navigationOptions: {
+        title: "Add Deck"
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: white,
+      inactiveTintColor: lightText,
+      indicatorStyle: {
+        backgroundColor: white
+      },
+      style: {
+        backgroundColor: black,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    },
+    animationEnabled: true,
+    swipeEnabled: true
+  }
+)
+
 const App = () => (
   <View style={{ flex: 1 }}>
-    <AppStatusBar backgroundColor={primary} barStyle="light-content" />
-    <NewCard />
+    <AppStatusBar backgroundColor={black} barStyle="light-content" />
+    <Navigation />
   </View>
 )
 
